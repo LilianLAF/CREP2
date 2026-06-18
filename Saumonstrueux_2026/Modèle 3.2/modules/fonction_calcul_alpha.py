6cf734928640f7aecbfedbf1266959cf4d7e772a
B1lel <bilel.mailautre@gmail.com>
2026-06-18 15:58:00 +0200

    Modif structure de code couche atmo et fn calcul alpha
    On calcule la concentration dans code couche atmo

--- Saumonstrueux_2026/Modèle 3.2/modules/fonction_calcul_alpha.py
+++ Saumonstrueux_2026/Modèle 3.2/modules/fonction_calcul_alpha.py
@@ -11,6 +11,21 @@ def concentration_CO2(annee):
         return 278  # valeur moyenne avant l'ère industrielle
     else:
         return 1.9 * annee - 3430  # modèle linéaire post-1952
+
+def concentration_CO2_altitude_annee(altitude, annee):
+    """
+    Renvoie la proportion de CO₂ en ppm pour une altitude et une année données.
+    Combine le profil altitudinal (référence 2000) et l'évolution temporelle.
+    """
+    if 0 <= altitude <= 60:
+        prop_altitude = 380
+    elif altitude > 132:
+        prop_altitude = 0
+    else:
+        prop_altitude = -5.26 * altitude + 736
+    coef = concentration_CO2(annee) / concentration_CO2(2000)
+    return prop_altitude * coef
+
     
 def concentration_O3(annee):
     return 
@@ -27,14 +42,14 @@ def concentration_H2O(annee):
 def calcul_alpha(P_emis, annee):
     flux_emis_terre = P_emis #en W/m^2
 
-    taux_co2_ppm = concentration_CO2(annee)#en ppm
-    taux_o3_ppm = concentration_O3(annee)#en ppm
-    taux_n2o_ppm = concentration_N2O(annee)#en ppm
-    taux_ch4_ppm = concentration_CH4(annee)#en ppm
-    taux_h2o_ppm = concentration_H2O(annee)#en ppm
+    taux_o3 = concentration_O3(annee)
+    taux_n2o = concentration_N2O(annee)
+    taux_ch4 = concentration_CH4(annee)
+    taux_h2o = concentration_H2O(annee)
 
-    lambda_range, z_range, upward_flux, optical_thickness, mean_flux_top = c_a.simulate_radiative_transfer(taux_co2_ppm, taux_o3_ppm, taux_n2o_ppm, taux_ch4_ppm, taux_h2o_ppm)
-    
-    alpha = mean_flux_top/flux_emis_terre
-    
-    return(alpha)
+    lambda_range, z_range, upward_flux, optical_thickness = c_a.simulate_radiative_transfer(annee, taux_o3, taux_n2o, taux_ch4, taux_h2o)
+
+    mean_flux_top = upward_flux[-1, :].sum()
+    alpha = mean_flux_top / flux_emis_terre
+
+    return alpha
