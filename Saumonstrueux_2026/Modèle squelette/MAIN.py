d53b99d8f2c5ce673bf859bc5e1c3ab013700407
LilianLAF <lilian.laffont@enise.fr>
2026-06-19 13:47:35 +0200

    update

--- Saumonstrueux_2026/Modèle squelette/MAIN.py
+++ Saumonstrueux_2026/Modèle squelette/MAIN.py
@@ -11,7 +11,7 @@ import Visualisation as Visu
 # ---------------------- Simulation Température ---------------------- #
 def temp(lat = 48.85, long = 2.35):
     cm, rho = 4180, 1000   # kJ/kg/K, kg
-    alpha = 0.0
+    alpha = 2
     d = 0.1 #10cm
     P0 = 1360  # W/m² – zenith irradiance at the top of the atmosphere
     PHI = 0.409  # precession angle rad  (23.45 deg)
